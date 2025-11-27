8a6060ce0edc7b6d121297fd1de3e96c86ef2af3
abhishek.sankhla <abhisheksankhl121@gmail.com>
2025-11-27 05:55:29 +0000

    fix nodemon

--- controller/urlRedirect.js
+++ controller/urlRedirect.js
@@ -5,7 +5,7 @@ import ErrorHandler from "../utils/errorHandler.js";
 export const urlRedirect = catchAsyncError(async(req,res,next)=>{
     const {id} = req.params
     const checkCodeexist = await Url.findOne({codeId:id})
-    if(!checkCodeexist)return next(new ErrorHandler("no longer redirect.", 404))
+    if(!checkCodeexist)return next(new ErrorHandler("no longer redirect...", 404))
     const response = await Url.findOneAndUpdate({ codeId:id },{$push:{
         history:{
             timestamp: Date.now()
